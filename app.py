b620952ea91370b3943fad5cf6ce59bea54eea4d
leslygaul <92374932+leslygaul@users.noreply.github.com>
2024-05-29 22:56:48 +0700

    .

--- app.py
+++ app.py
@@ -1,39 +1,30 @@
-#C:\flask_dev\myapp\app.py
-from flask import Flask, render_template, request, redirect, url_for, session, abort, jsonify
-from flask_bcrypt import Bcrypt #pip install Flask-Bcrypt = https://pypi.org/project/Flask-Bcrypt/
+from flask import Flask, render_template, request, redirect, url_for, session, abort, jsonify, send_file
+from flask_bcrypt import Bcrypt
 from werkzeug.utils import secure_filename
 import os
 import re
-import sqlalchemy
+from io import BytesIO
 from models import db, Users, Barang
-   
+
 app = Flask(__name__)
-    
 app.config['SECRET_KEY'] = 'siapa-ini'
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flaskdb.db'
-# Databse configuration mysql                             Username:password@hostname/databasename
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://kplesly_owner:MOSGwn3K9ZVE@ep-lively-hat-a1dd4nff.ap-southeast-1.aws.neon.tech/kplesly?sslmode=require'
-  
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-SQLALCHEMY_ECHO = True
-   
-bcrypt = Bcrypt(app) 
-  
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+
+bcrypt = Bcrypt(app)
 db.init_app(app)
-         
+
 with app.app_context():
     db.create_all()
- 
- 
-app.config['UPLOAD_FOLDER'] = 'kplesly_owner'
+
 shopping_cart = []
-    
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
-    
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+
 def allowed_file(filename):
- return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
- 
-@app.route("/", methods =['GET', 'POST'])
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route("/", methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
 
@@ -46,28 +37,15 @@ def shop():
     barangs = Barang.query.all()
     return render_template('clients.html', barangs=barangs)
 
-
 @app.route('/add-to-cart', methods=['POST'])
 def add_to_cart():
     data = request.get_json()
-
-    item = {
-        'id': data['id'],
-        'nama': data['nama'],
-        'harga': data['harga'],
-        'quantity': 1  # Default quantity set to 1
-    }
-
-    # Cek apakah item sudah ada di keranjang
+    item = {'id': data['id'], 'nama': data['nama'], 'harga': data['harga'], 'quantity': 1}
     existing_item = next((i for i in shopping_cart if i['id'] == item['id']), None)
-
     if existing_item:
-        # Jika item sudah ada, tambahkan jumlahnya
         existing_item['quantity'] += 1
     else:
-        # Jika item belum ada, tambahkan ke keranjang
         shopping_cart.append(item)
-
     return jsonify({'status': 'success'})
 
 @app.route('/get-cart', methods=['GET'])
@@ -81,10 +59,7 @@ def register():
         fullname = request.form['name']
         password = request.form['password']
         email = request.form['email']
-        
-        # Check if the email already exists
         user_exists = Users.query.filter_by(email=email).first() is not None
-        
         if user_exists:
             mesage = 'Email already exists!'
         elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
@@ -92,16 +67,10 @@ def register():
         elif not fullname or not password or not email:
             mesage = 'Please fill out the form!'
         else:
-            # Assign the default role
             role = 'user'
-            
-            # Check if a special condition is met (e.g., specific email address) to assign the 'admin' role
             if email == 'admin@gmail.com':
                 role = 'admin'
-            
-            # hashed_password = bcrypt.generate_password_hash(password)
             new_user = Users(name=fullname, email=email, password=password, role=role)
-            
             db.session.add(new_user)
             db.session.commit()
             mesage = 'You have successfully registered!'
@@ -109,19 +78,16 @@ def register():
         mesage = 'Please fill out the form!'
     return render_template('register.html', mesage=mesage)
 
-     
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     message = ''
     if request.method == 'POST':
         email = request.form['email']
         password = request.form['password']
-
         if email == '' or password == '':
             message = 'Please enter email and password!'
         else:
             user = Users.query.filter_by(email=email).first()
-
             if user is None:
                 message = 'Please enter correct email/password!'
             else:
@@ -129,59 +95,32 @@ def login():
                 session['userid'] = user.id
                 session['name'] = user.name
                 session['email'] = user.email
-
-                    # Set the 'role' based on email
                 if email == 'admin@gmail.com':
                     session['role'] = 'admin'
                 else:
                     session['role'] = 'user'
-
                 message = 'Logged in successfully!'
                 return redirect(url_for('dashboard'))
-                # if not bcrypt.check_password_hash(user.password, password):
-                #     message = 'Please enter correct email and password!'
-                # else:
-                #     session['loggedin'] = True
-                #     session['userid'] = user.id
-                #     session['name'] = user.name
-                #     session['email'] = user.email
-
-                #     # Set the 'role' based on email
-                #     if email == 'admin@gmail.com':
-                #         session['role'] = 'admin'
-                #     else:
-                #         session['role'] = 'user'
-
-                #     message = 'Logged in successfully!'
-                #     return redirect(url_for('dashboard'))
-
     return render_template('login.html', message=message)
 
- 
-@app.route("/dashboard", methods =['GET', 'POST'])
+@app.route("/dashboard", methods=['GET', 'POST'])
 def dashboard():
-    if 'loggedin' in session:        
+    if 'loggedin' in session:
         return render_template("dashboard.html")
-    return redirect(url_for('login'))   
- 
+    return redirect(url_for('login'))
+
 @app.route('/logout')
 def logout():
     session.pop('loggedin', None)
     session.pop('userid', None)
     session.pop('email', None)
     return redirect(url_for('login'))
- 
-# Manage Barang
+
 @app.route("/barang", methods=['GET', 'POST'])
 def barang():
-    if 'loggedin' in session:
-        # Check if the user has the "admin" role
-        if 'role' in session and session['role'] == 'admin':
-            barangs = Barang.query.all()
-            return render_template("barang.html", barangs=barangs)
-        else:
-            # If the user doesn't have the "admin" role, abort with a 403 Forbidden error
-            abort(403)
+    if 'loggedin' in session and session.get('role') == 'admin':
+        barangs = Barang.query.all()
+        return render_template("barang.html", barangs=barangs)
     return redirect(url_for('login'))
 
 @app.route('/save_barang', methods=['POST'])
@@ -194,55 +133,41 @@ def save_barang():
             stok = request.form['stok']
             kategori = request.form['kategori']
             action = request.form['action']
-
             if action == 'updateBarang':
                 barangid = request.form['barangid']
                 barang = Barang.query.get(barangid)
-
                 barang.nama = nama
                 barang.harga = harga
                 barang.stok = stok
                 barang.kategori = kategori
-
-                # Check if a new image is uploaded
                 if 'picture' in request.files:
                     file = request.files['picture']
                     if file.filename != '' and allowed_file(file.filename):
                         filename = secure_filename(file.filename)
-                        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                        file_data = file.read()
                         barang.picture = filename
-                # Commit changes to the database
+                        barang.picture_data = file_data
+                        barang.picture_mime_type = file.mimetype
                 db.session.commit()
-                print("UPDATE barang")
             else:
                 file = request.files['picture']
-                filename = secure_filename(file.filename)
-                print(filename)
                 if file and allowed_file(file.filename):
-                    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-                    filenameimage = file.filename
-
-                    barang = Barang(nama=nama, harga=harga, stok=stok, picture=filenameimage, kategori=kategori)
+                    filename = secure_filename(file.filename)
+                    file_data = file.read()
+                    barang = Barang(nama=nama, harga=harga, stok=stok, picture=filename, picture_data=file_data, picture_mime_type=file.mimetype, kategori=kategori)
                     db.session.add(barang)
                     db.session.commit()
-                    print("INSERT INTO barang")
                 else:
                     msg = 'Invalid Upload, only png, jpg, jpeg, gif allowed'
             return redirect(url_for('barang'))
-        elif request.method == 'POST':
-            msg = 'Please fill out the form!'
-        return render_template("barang.html", msg=msg)
+        msg = 'Please fill out the form!'
     return redirect(url_for('login'))
-
 
 @app.route("/edit_barang", methods=['GET', 'POST'])
 def edit_barang():
-    msg = ''
     if 'loggedin' in session:
         barangid = request.args.get('barangid')
-        print(barangid)
         barang = Barang.query.get(barangid)
-
         return render_template("edit_barang.html", barang=barang)
     return redirect(url_for('login'))
 
@@ -251,12 +176,17 @@ def delete_barang():
     if 'loggedin' in session:
         barangid = request.args.get('barangid')
         barang = Barang.query.get(barangid)
-        print(barang.picture)
         db.session.delete(barang)
         db.session.commit()
-        os.unlink(os.path.join(app.config['UPLOAD_FOLDER'], barang.picture))
         return redirect(url_for('barang'))
     return redirect(url_for('login'))
 
-if __name__=='__main__':
+@app.route('/images/<int:barang_id>')
+def image(barang_id):
+    barang = Barang.query.get(barang_id)
+    if barang and barang.picture_data:
+        return send_file(BytesIO(barang.picture_data), mimetype=barang.picture_mime_type)
+    return abort(404)
+
+if __name__ == '__main__':
     app.run(debug=True)
--- models.py
+++ models.py
@@ -1,21 +1,22 @@
-#C:\flask_dev\myapp\models.py
+# C:\flask_dev\myapp\models.py
 from flask_sqlalchemy import SQLAlchemy
-          
+
 db = SQLAlchemy()
-          
+
 class Users(db.Model):
-    __tablename__ = "user"
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(150), index=True, unique=True)
-    email = db.Column(db.String(150), index=True, unique=True)
-    password = db.Column(db.String(255), index=True, unique=True)
+    name = db.Column(db.String(50))
+    email = db.Column(db.String(50), unique=True)
+    password = db.Column(db.String(200))
     role = db.Column(db.String(50))
 
 class Barang(db.Model):
-    __tablename__ = "barang"
     id = db.Column(db.Integer, primary_key=True)
-    nama = db.Column(db.String(100), nullable=False)
-    harga = db.Column(db.Float, nullable=False)
-    stok = db.Column(db.Integer, nullable=False)
-    picture = db.Column(db.String(150), index=True, unique=True)
-    kategori = db.Column(db.String(50), nullable=False)
+    nama = db.Column(db.String(100))
+    harga = db.Column(db.Integer)
+    stok = db.Column(db.Integer)
+    kategori = db.Column(db.String(100))
+    picture = db.Column(db.String(100))  # Filename for the picture
+    picture_data = db.Column(db.LargeBinary)  # Binary data of the picture
+    picture_mime_type = db.Column(db.String(50))  # MIME type of the picture
+
